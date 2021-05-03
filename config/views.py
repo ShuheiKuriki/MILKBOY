04fa80f0f2fb09217e1948ce51cd33c5f7465ee4
ShuheiKuriki <shukuri.7336.8@gmail.com>
2021-05-03 09:40:09 +0900

    small adjust

--- config/views.py
+++ config/views.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 
 def index(request):
     genres = ['日本の映画', '日本の漫画', 'アニメ', '日本のゲーム', '日本の歴史', '日本の地理', '日本のスポーツ', '日本の音楽',
-              '物質', '数学的対象', '計算機科学', 'アルゴリズムとデータ構造']
+              '物質', '物理現象', '数学的対象', '計算機科学', 'アルゴリズムとデータ構造']
     return render(request, 'index.html', {'genres': genres})
 # Create your views here.
