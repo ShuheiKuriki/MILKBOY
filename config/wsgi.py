8b86c6a6dd43ddd4394ceedb31a0dc4c6f477f11
ShuheiKuriki <shukuri.7336.8@gmail.com>
2021-05-14 00:59:23 +0900

    add genre

--- config/wsgi.py
+++ config/wsgi.py
@@ -222,11 +222,13 @@ def introduction(category, pred1, pred2):
 
 
 def daily():
+    schedule.every().day.at("06:00").do(tweet)
     schedule.every().day.at("09:00").do(tweet)
     schedule.every().day.at("12:00").do(tweet)
     schedule.every().day.at("15:00").do(tweet)
     schedule.every().day.at("18:00").do(tweet)
     schedule.every().day.at("21:00").do(tweet)
+    schedule.every().day.at("00:00").do(tweet)
     while True:
         schedule.run_pending()
         time.sleep(300)
